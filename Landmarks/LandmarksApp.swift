2ab4a7ff9a6ca16245e6cfccd08fa1d6fa478111
Chew Lee <leeyingqiu@gmail.com>
2021-04-18 12:10:31 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yingqiu Lee on 4/18/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
